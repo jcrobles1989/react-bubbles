42cc934208223810d8d5237b76d8acd873992745
Jose Carlos Robles <jcrobles1989@gmail.com>
2020-07-24 10:54:07 -0700

    login form built

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,8 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Login = () => {
+const [username, setUsername] = useState("");
+const [password, setPassword] = useState("");
+
+handleChanges = (e) => {
+  setUsername({
+    ...username,
+    [e.target.name]: e.target.value
+  })
+  setPassword({
+    ...password,
+    [e.target.name]: e.target.value
+  })
+}
+
+const Login = (e) => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+  e.preventDefault();
+  axios.post("http://localhost:5000/api/login", username, password)
+  .then((res) => {
+    localStorage.setItem("token", res.data.payload);
+    this.PaymentResponse.history.push("/colors")
+  })
+  .catch((err) => console.log(err));
+
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
